782cf2cf0ad230ea97b0856be9c04bd3e09bb81f
Shurpaev Gadji-Ata <shurpaevgadjiata@gmail.com>
2025-11-01 11:36:06 +0300

    Update InpytLayer.cs

--- MO_31_2_Shurpaev_Elizabeth-homework/MO_31_2_Shurpaev_Elizabeth/NeiroNet/InpytLayer.cs
+++ MO_31_2_Shurpaev_Elizabeth-homework/MO_31_2_Shurpaev_Elizabeth/NeiroNet/InpytLayer.cs
@@ -3,20 +3,14 @@ using System.IO;
 
 namespace MO_31_2_Shurpaev_Elizabeth.NeiroNet
 {
-    // Предполагается, что где-то определен enum NetworkMode
-    public enum NetworkMode { Train, Test }
-
     class InpytLayer
     {
-        //поля
         private double[,] trainset;
         private double[,] testset;
 
-        //свойства (Исправлена опечатка в имени свойства Teatset -> Testset)
         public double[,] Trainset { get => trainset; }
-        public double[,] Testset { get => testset; } // Исправлено
+        public double[,] Testset { get => testset; }
 
-        // Конструктор
         public InpytLayer(NetworkMode nm)
         {
             string path = AppDomain.CurrentDomain.BaseDirectory;
@@ -26,30 +20,23 @@ namespace MO_31_2_Shurpaev_Elizabeth.NeiroNet
             switch (nm)
             {
                 case NetworkMode.Train:
-                    // В режиме Train читаем train.txt
                     tmpArrStr = File.ReadAllLines(path + "train.txt");
-                    // 16 колонок, где последние колонки обычно содержат целевые значения
-                    trainset = new double[tmpArrStr.Length, 16]; 
+                    trainset = new double[tmpArrStr.Length, 16];
 
                     for (int i = 0; i < tmpArrStr.Length; i++)
                     {
-                        // Разделитель - пробел
-                        tmpStr = tmpArrStr[i].Split(' '); 
+                        tmpStr = tmpArrStr[i].Split(' ');
 
                         for (int j = 0; j < 16; j++)
                         {
                             trainset[i, j] = double.Parse(tmpStr[j]);
                         }
                     }
-                    // Перетасовка обучающей выборки
                     Shuffling_Array_Rows(trainset);
                     break;
 
                 case NetworkMode.Test:
-                    // В режиме Test, возможно, нужно читать test.txt, 
-                    // но согласно Вашему коду, читается 'train.txt'
-                    // Оставляем как в оригинале, но обычно здесь должен быть 'test.txt'
-                    tmpArrStr = File.ReadAllLines(path + "train.txt"); 
+                    tmpArrStr = File.ReadAllLines(path + "train.txt");
                     testset = new double[tmpArrStr.Length, 16];
 
                     for (int i = 0; i < tmpArrStr.Length; i++)
@@ -61,50 +48,41 @@ namespace MO_31_2_Shurpaev_Elizabeth.NeiroNet
                             testset[i, j] = double.Parse(tmpStr[j]);
                         }
                     }
-                    // Перетасовка тестовой выборки
                     Shuffling_Array_Rows(testset);
                     break;
             }
         }
 
-        /// <summary>
-        /// Перетасовывает строки двумерного массива (выборки) 
-        /// методом Фишера-Йетса.
-        /// </summary>
-        /// <param name="arr">Двумерный массив (выборка).</param>
         public void Shuffling_Array_Rows(double[,] arr)
         {
-            // Получаем количество строк в массиве
             int rows = arr.GetLength(0);
-            // Получаем количество столбцов (длина строки/примера)
-            int cols = arr.GetLength(1); 
-
-            // Используем статический экземпляр Random (лучше, чем создавать новый в цикле)
+            int cols = arr.GetLength(1);
             Random rand = new Random();
 
-            // Алгоритм Фишера-Йетса
-            // Идем с конца до второй строки (i > 0)
             for (int i = rows - 1; i > 0; i--)
             {
-                // Генерируем случайный индекс j от 0 до i (включительно)
                 int j = rand.Next(i + 1);
 
-                // Если j != i, меняем местами текущую строку arr[i, ...] со случайной строкой arr[j, ...]
                 if (i != j)
                 {
-                    // Временный массив для хранения текущей строки arr[i, ...]
-                    double[] tempRow = new double[cols]; 
+                    double[] tempRow = new double[cols];
 
-                    // 1. Копируем arr[i, ...] во tempRow
                     for (int k = 0; k < cols; k++)
                     {
                         tempRow[k] = arr[i, k];
                     }
 
-                    // 2. Копируем arr[j, ...] в arr[i, ...]
                     for (int k = 0; k < cols; k++)
                     {
                         arr[i, k] = arr[j, k];
                     }
 
-                    // 3.
+                    for (int k = 0; k < cols; k++)
+                    {
+                        arr[j, k] = tempRow[k];
+                    }
+                }
+            }
+        }
+    }
+}
